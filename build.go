05de1c49a4a701993bb310cc15c0a325652460ab
Karl Heinz Marbaise <kama@soebes.de>
2021-02-07 11:37:20 +0100

    Added License information.

--- build.go
+++ build.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The GitJiraHelper Authors. All rights reserved.
+// Copyright 2021 The disco Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 //+build vendor
